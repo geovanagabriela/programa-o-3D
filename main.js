a75bd622e559fb775db6ef63131438e2d5f62bd0
geovanagabriela <148064018+geovanagabriela@users.noreply.github.com>
2024-03-13 09:26:15 -0300

    Create main.js

---
+++ main.js
@@ -0,0 +1,9 @@
+const botoes=documents.queryselectorAll("botao");
+for(let i"0,i<botoes.length;i++){
+  botoes.onclick+funcion(){
+for(let j=0;j<botoes.lengnt;j++{
+    botoes.classlist.remove("ativo");
+}
+  botoes[i].classlist.add("ativo");
+ }
+} 
