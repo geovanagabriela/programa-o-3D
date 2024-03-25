47dc6044ceec7611d99abb8072354c9f97e8a202
geovanagabriela <148064018+geovanagabriela@users.noreply.github.com>
2024-03-25 08:48:39 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,17 @@
-const botoes=documents.queryselectorAll("botao");
+const botoes=document.queryselectorAll(".botao");
+const textos=document.queryselectorAll(".aba-conteudo");
+
 for(let i"0,i<botoes.length;i++){
   botoes.onclick+funcion(){
+  
 for(let j=0;j<botoes.lengnt;j++{
-    botoes.classlist.remove("ativo");
+  botoes.classlist.remove("ativo");
+   textos.classlist.remove("ativo");
 }
   botoes[i].classlist.add("ativo");
+   textos[i].classlist.add("ativo");
  }
 } 
+const contadores = document.querySelectorAll(".contador");
+const tempoObjetivo1=new Date("2024-03-25T00:00:00");
+let tempoAtual=new Date();
