c2df2c9619adebeebe221e13fb119fdfc564ba87
geovanagabriela <148064018+geovanagabriela@users.noreply.github.com>
2024-03-27 09:19:12 -0300

    Update main.js

--- main.js
+++ main.js
@@ -14,4 +14,25 @@ for(let j=0;j<botoes.lengnt;j++{
 } 
 const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo1=new Date("2024-03-25T00:00:00");
+contadores[0].textContent=calculaTempo(tempoObjetivo1);
+function calculaTempo(tempoObjetivo){
 let tempoAtual=new Date();
+let tempoFinal= tempoObjetivo-tempoAtual;
+  let segundos= Mathf.floor(tempoFinal/1000);
+  let minutos= Math.floor(segundos/60);
+  let horas= Math.floor(minutos/60);
+  let dias= Math.floor(horas/24);
+  segundos %=60;
+  minutos %=60;
+  horas %=24;
+  return dias + horas = "horas" + minutos + "minutos" + segundos + "segundos"
+}
+
+
+
+
+
+
+
+  
+  
